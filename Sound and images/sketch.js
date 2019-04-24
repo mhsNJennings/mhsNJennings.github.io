4b3acf3e6501494ed2b22575450ee81dcb1797f6
mhsNJennings <48356613+mhsNJennings@users.noreply.github.com>
2019-04-24 09:07:19 -0400

    Update sketch.js

--- Sound and images/sketch.js
+++ Sound and images/sketch.js
@@ -1,7 +1,7 @@
 var mySound; // Creates a variable to hold the sound
 var img;
   function preload() {
-    //mySound = loadSound('');  // Loads the sound file into the variable
+    mySound = loadSound('Phone Ringing.mp3');  // Loads the sound file into the variable
     //img = loadImage('source.gif');  //Will only load first frame
     img = createImg('source.gif');
   }
@@ -10,6 +10,6 @@ var img;
     createCanvas(1280, 960);
     image(img, 640, 480);
     // Notice that you have to put the variable name in front of the functions
-    //mySound.setVolume(0.1);
-    //mySound.play();
+    mySound.setVolume(0.1);
+    mySound.play();
   }
